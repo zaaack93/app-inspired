8e44aa61c1bc700a20dc6cab0326e06976f588f2
zbouzangad <bouzanagdzakaria@gmail.com>
2022-04-09 13:46:03 +0000

    add vuejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+import navbar from "./layouts/navbar.vue"
+
+const app = new Vue({
+    el: '#app',
+    components: { navbar }
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,9 @@
 @extends('shopify-app::layouts.default')
 
 @section('content')
-    <!-- You are: (shop domain name) -->
-    <p>You are: {{ $shopDomain ?? Auth::user()->name }}</p>
+    <div id="app">
+        <navbar></navbar>
+    </div>
 @endsection
 
 @section('scripts')
